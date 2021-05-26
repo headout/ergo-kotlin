303b1768e86930b97381a76eaed5a32521944351
betterclever <paliwal.pranjal83@gmail.com>
2021-05-26 11:16:29 +0530

    feat: Change to updated serialization APIs
    Signed-off-by: Shivansh Saini <shivanshs9@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,15 @@ allprojects {
     ext {
         set("deps.auto", "com.google.auto.service:auto-service:1.0-rc7")
         set("deps.kotlin-metadata", "me.eugeniomarletti.kotlin.metadata:kotlin-metadata:1.4.0")
-        set("deps.kotlinpoet", "com.squareup:kotlinpoet:1.5.0")
-        set("deps.kotlinpoet-metadata", "com.squareup:kotlinpoet-metadata:1.5.0")
+        set("deps.kotlinpoet", "com.squareup:kotlinpoet:1.8.0")
+        set("deps.kotlinpoet-metadata", "com.squareup:kotlinpoet-metadata:1.8.0")
         set("deps.truth", "com.google.truth:truth:1.0.1")
         set("deps.junit", "junit:junit:4.13")
         set("deps.compile-testing", "com.github.tschuchortdev:kotlin-compile-testing:1.2.8")
-        set("deps.serialization", "org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
-        set("deps.coroutine", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-        set("deps.coroutine-jdk8", "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.7")
+//        set("deps.serialization", "org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+        set("deps.serialization", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.21.0")
+        set("deps.coroutine", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+        set("deps.coroutine-jdk8", "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.0")
     }
 }
 
